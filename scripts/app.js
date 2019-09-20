3fbd10ba2c3d0fb2513af376cb649dc8a37a6dfd
mattdavey <matt.davey540@me.com>
2019-09-20 14:40:23 +0100

    merged js.game with js.app

--- scripts/app.js
+++ scripts/app.js
@@ -52,6 +52,9 @@ document.addEventListener('DOMContentLoaded', () => {
       stage2.classList.add('hide')
       stage3.classList.add('hide')
       cannon.classList.add('hide')
+      document.removeEventListener('keydown', (e) => {
+        console.log('game')
+      })
     }, 1000)
     setTimeout(function () {
       stage0.classList.remove('hide')
@@ -89,5 +92,87 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     })//end of event listener
   )//end of modebutton
+
+  // **************************************//
+  // ************ GAME LOGIC **************//
+  // **************************************//
+
+  const play = document.querySelector('#play')
+  //SHOW WHERE THE PLAYER IS BY UPDATING CLASS
+  function handleClick(e) {
+    e.target.classList.add('player')
+  }
+
+  //CHECK GAME HAS STARTED BEFORE LOADING GRID AND
+  //BEFORE RECORDING MOVES AND ATTACKS
+  play.addEventListener('click', () => {
+    console.log('game script enabled')
+
+    //RULES OF THE GRID
+    const grid = document.querySelector('.grid')
+    const width = 18
+    const cells = []
+    let playerIdx = 81
+  
+
+    //GENERATE 9 x 18 GAMEBOARD
+    for (let i = 0; i < width * 9; i++) {
+      const cell = document.createElement('div')
+      grid.appendChild(cell)
+      cells.push(cell)
+      cell.addEventListener('click', handleClick)
+
+    }//END OF GAME BOARD GEN
+
+
+    //START PLAYER @ playerIdx
+    cells[playerIdx].classList.add('player')
+
+    //KEY DOWN LISTENER >> MOVE PLAYER
+    document.addEventListener('keydown', (e) => {
+      cells[playerIdx].classList.remove('player')
+      const x = playerIdx % width
+      const y = Math.floor(playerIdx / width)
+      switch (e.keyCode) {
+        //left
+        case 37: if (x > 0) playerIdx -= 1
+          break
+        case 65: if (x > 0) playerIdx -= 1
+          break
+        // up  
+        case 38: if (y > 0) playerIdx -= width
+          break
+        case 87: if (y > 0) playerIdx -= width
+          break
+        // right
+        case 39: if (x < width - 1) playerIdx += 1
+          break
+        case 68: if (x < width - 1) playerIdx += 1
+          break
+        //down  
+        case 40: if (y < 8) playerIdx += width
+          break
+        case 83: if (y < 8) playerIdx += width
+          break
+      }
+      cells[playerIdx].classList.add('player')
+    })//END OF KEY DOWN LISTENER
+
+    // ATTACK!
+    document.addEventListener('keydown', (e) => {
+
+      if (e.keyCode === 32) {
+        cells[playerIdx].classList.add('attack')
+        console.log(e.keyCode)
+      }
+
+      // add if statement, if attack hit = true
+
+
+    })//END OF ATTACK
+
+
+
+  })
 
 })
